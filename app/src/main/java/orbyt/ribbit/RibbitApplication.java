5ddf07ae6d493381e2b44d29bccff5b69913e65f
Orbyt <pbfreak007@gmail.com>
2015-07-20 20:19:54 -0400

    Parse added and testing successful

--- app/src/main/java/orbyt/ribbit/RibbitApplication.java
+++ app/src/main/java/orbyt/ribbit/RibbitApplication.java
@@ -1,7 +1,19 @@
 package orbyt.ribbit;
 
+import android.app.Application;
+
+import com.parse.Parse;
+
 /**
  * Created by orbyt on 7/20/2015.
  */
-public class RibbitApplication {
+public class RibbitApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        Parse.enableLocalDatastore(this);
+
+        Parse.initialize(this, "J5Z8DdWopDZoaWp8ENRXlxSpHtbAEtQDNox9W2vu", "RUdDu92UQzGbSky8rFghGd4J6BuxLAiatjv6namk");
+    }
+
 }
