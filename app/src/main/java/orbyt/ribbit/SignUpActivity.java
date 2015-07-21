a94dd025d86bd94b5e4f6273c862bc4a517b03d4
Orbyt <pbfreak007@gmail.com>
2015-07-20 20:43:44 -0400

    Added a check in sign up activity to make sure all fields are filled out.

--- app/src/main/java/orbyt/ribbit/SignUpActivity.java
+++ app/src/main/java/orbyt/ribbit/SignUpActivity.java
@@ -1,17 +1,55 @@
 package orbyt.ribbit;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.AlertDialog;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 
 public class SignUpActivity extends ActionBarActivity {
+
+    @Bind(R.id.usernameField) EditText mUsername;
+    @Bind(R.id.passwordField) EditText mPassword;
+    @Bind(R.id.emailField) EditText mEmail;
+    @Bind(R.id.signUpButton) Button mSignUpButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        ButterKnife.bind(this);
+
+        mSignUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = mUsername.getText().toString().trim();
+                String password = mPassword.getText().toString().trim();
+                String email = mEmail.getText().toString().trim();
+
+                if (username.isEmpty() || password.isEmpty() || email.isEmpty()) {
+
+                    AlertDialog.Builder builder = new AlertDialog.Builder(SignUpActivity.this);
+                    builder.setMessage(R.string.sign_up_error_message)
+                        .setTitle(R.string.sign_up_error_title)
+                        .setPositiveButton(android.R.string.ok, null);
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                } else {
+                    //create new user
+                }
+
+
+            }
+        });
     }
 
     @Override
